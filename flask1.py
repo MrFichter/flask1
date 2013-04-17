dcfef9c9bdf96e1a388c5e5911eafeec00e669aa
admin <jonathan@ubuntu.(none)>
2013-04-17 05:47:48 -0400

    Important learning notes. Minor changes. Here are some #learningnote #learning note notes. I learned that any .py file can act as a module, and that you can import things from it. __name__ is a magic variable that plays a role here. The answer to a reddit question I asked explains more about this.

--- flask1.py
+++ flask1.py
@@ -4,6 +4,7 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from contextlib import closing
 
 # configuration
 DATABASE = '/tmp/flask1.db'
@@ -22,8 +23,18 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-#The line below lets you run the file as a standalone application.
-#(If you want.)
+#initialize database
+
+def init_db():
+	with closing (connect_db()) as db:
+		with app.open_resource('schema.sql') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+
+#The line below checks "if we are running the module by itself or importing.
+#See explanation here: http://marakana.com/forums/python/python/116.html
+ 
 if __name__ == '__main__':
 	app.run()
 
